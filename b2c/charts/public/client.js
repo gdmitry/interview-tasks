5fb7f5e97a04eca56b49f14cafd7af2632e83d07
Dmitry Goncharenko <dmitry.goncharenko24@gmail.com>
2016-09-05 01:22:21 +0300

    graphics changes

--- b2c/charts/public/client.js
+++ b2c/charts/public/client.js
@@ -1,17 +1,57 @@
 'use strict';
 
-var endpoints = {
-    baseUrl: 'http://127.0.0.1:3000',
-    configUrl: baseUrl + '/api/v1/config',
-    pointsUrl: baseURl + '/api/v1/points'
-}
+var config = {
+    endpoints: {
+        configUrl: 'http://127.0.0.1:3000/api/v1/config',
+        pointsUrl: 'http://127.0.0.1:3000/api/v1/points'
+    }
+};
+
+var Graphics = {
+    ctx: null,
+    canvas: null,
+    init: function () {
+        this.canvas = document.getElementById('graph');
+        if (this.canvas.getContext) {
+            this.ctx = this.canvas.getContext('2d');
+        } else {
+            throw new Error('Canvas is unsupported.');
+        }
+    },
+    clear: function () {
+        this.canvas.width = this.canvas.width;
+    },
+    drawPoints: function (points) {
+        var length = points.length;
+        var diffY = config.points.MAX - config.points.MIN;
+        var ratioX = this.canvas.width / config.points.QTY;
+        var ratioY = this.canvas.height / diffY;
+        var ctx = this.ctx;
+        var i;
+
+        this.clear();
+        ctx.beginPath();
+        ctx.fillStyle = '#000';
+        ctx.moveTo(0, (points[i] + diffY / 2) * ratioY);
+        for (i = 1; i < length; i++) {
+            ctx.lineTo(i * ratioX, (points[i] + diffY / 2) * ratioY);
+        }
+        ctx.stroke();
+    }
+};
 
 function httpGet(url) {
-    return httpRequest(url, 'GET');
+    return httpRequest(url, 'GET')
+        .then(JSON.parse);
+}
+
+function updateConfig(data) {
+    config.points = data.POINTS;
+    return config;
 }
 
 function getUpdateInterval(config) {
-    return config.UPDATE_INTERVAL;
+    return config.points.UPDATE_INTERVAL;
 }
 
 function delay(interval) {
@@ -20,39 +60,29 @@ function delay(interval) {
     });
 }
 
-function drawPoints(points) {
-    var canvas = document.getElementById('graph');
-    var length = points.length;
-    var i;
+function updatePoints(interval) {
+    var pointsUrl = config.endpoints.pointsUrl;
+    return httpGet(pointsUrl)
+        .then(Graphics.drawPoints.bind(Graphics))
+        .catch(console.error.bind(console))
+        .then(delay.bind(undefined, 2000))
+        .then(updatePoints);
 
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
-        // clear canvas
-        canvas.width = canvas.width;
-        ctx.beginPath();
-        ctx.fillStyle = '#000';
-        ctx.moveTo(0, 0);
-        for (i = 0; i < length; i++) {
-            ctx.lineTo(i, points[i]*15);
-        }
-        ctx.stroke();
-    } else {
-        throw new Error('Canvas is unsupported.');
-    }
 }
 
-function updatePoints(interval) {
-    var pointsUrl = endpoints.pointsUrl;
-    return httpGet(pointsUrl)
-        .then(drawPoints)
-        .catch(console.error.bind(console))
-        .then(delay.bind(undefined, interval))
-        .then(updatePoints);
-       
+function startUpdate() {
+    var configUrl = config.endpoints.configUrl;
+    return httpGet(configUrl)
+        .then(updateConfig)
+        .then(getUpdateInterval)
+        .then(updatePoints)
+        .catch(console.error.bind(console));
 }
 
 // start
-httpGet(endpoints.configUrl)
-    .then(getUpdateInterval)
-    .then(updatePoints)
-    .catch(console.error.bind(console));
+try {
+    Graphics.init();
+    startUpdate();
+} catch (e) {
+    console.error(e);
+}
