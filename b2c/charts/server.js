94588c672ea2790cbc5c7e92c56371f74cd37919
Dmitry Goncharenko <dmitry.goncharenko24@gmail.com>
2016-09-04 23:57:11 +0300

    Fix CORS

--- b2c/charts/server.js
+++ b2c/charts/server.js
@@ -33,6 +33,12 @@ setInterval(updatePoints, CONFIG.POINTS.UPDATE_INTERVAL);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/v1/config', function (req, res) {
 	res.json(CONFIG);
 });
