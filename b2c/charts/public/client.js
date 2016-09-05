16768e8c4c3b7fb4db2093bcd60349fb04534323
Dmitry Goncharenko <dmitry.goncharenko24@gmail.com>
2016-09-06 00:20:31 +0300

    Use function constructor

--- b2c/charts/public/client.js
+++ b2c/charts/public/client.js
@@ -1,88 +1,101 @@
 'use strict';
 
-var config = {
-    endpoints: {
-        configUrl: 'http://127.0.0.1:3000/api/v1/config',
-        pointsUrl: 'http://127.0.0.1:3000/api/v1/points'
-    }
-};
-
-var Graphics = {
-    ctx: null,
-    canvas: null,
-    init: function () {
-        this.canvas = document.getElementById('graph');
-        if (this.canvas.getContext) {
-            this.ctx = this.canvas.getContext('2d');
-        } else {
-            throw new Error('Canvas is unsupported.');
+(function (httpRequest) {
+    var config = {
+        endpoints: {
+            configUrl: 'http://127.0.0.1:3000/api/v1/config',
+            pointsUrl: 'http://127.0.0.1:3000/api/v1/points'
+        },
+        canvas: {
+            width: 500,
+            height: 300
         }
-    },
-    clear: function () {
+    };
+
+    function CanvasController(id) {
+        this.ctx = null;
+        this.canvas = null;
+
+        (function init(id) {
+            this.canvas = document.getElementById(id);
+            if (this.canvas.getContext) {
+                this.ctx = this.canvas.getContext('2d');
+                this.canvas.width = config.canvas.width;
+                this.canvas.height = config.canvas.height;
+            } else {
+                throw new Error('Canvas is unsupported.');
+            }
+        }).call(this, id);
+    };
+
+    CanvasController.prototype.clear = function () {
         this.canvas.width = this.canvas.width;
-    },
-    drawPoints: function (points) {
+    };
+
+    CanvasController.prototype.drawPoints = function (points) {
         var length = points.length;
         var diffY = config.points.MAX - config.points.MIN;
         var ratioX = this.canvas.width / config.points.QTY;
         var ratioY = this.canvas.height / diffY;
-        var ctx = this.ctx;
         var i;
 
         this.clear();
-        ctx.beginPath();
-        ctx.fillStyle = '#000';
-        ctx.moveTo(0, (points[i] + diffY / 2) * ratioY);
+        this.ctx.beginPath();
+        this.ctx.fillStyle = '#000';
+        this.ctx.moveTo(0, (points[i] + diffY / 2) * ratioY);
         for (i = 1; i < length; i++) {
-            ctx.lineTo(i * ratioX, (points[i] + diffY / 2) * ratioY);
+            this.ctx.lineTo(i * ratioX, (points[i] + diffY / 2) * ratioY);
         }
-        ctx.stroke();
+        this.ctx.stroke();
+    };
+
+    function httpGet(url) {
+        return httpRequest(url, 'GET')
+            .then(JSON.parse);
     }
-};
 
-function httpGet(url) {
-    return httpRequest(url, 'GET')
-        .then(JSON.parse);
-}
+    function updateConfig(data) {
+        config.points = data.POINTS;
+        return config;
+    }
 
-function updateConfig(data) {
-    config.points = data.POINTS;
-    return config;
-}
+    function getUpdateInterval(config) {
+        return config.points.UPDATE_INTERVAL;
+    }
 
-function getUpdateInterval(config) {
-    return config.points.UPDATE_INTERVAL;
-}
+    function delay(interval) {
+        return new Promise(function (resolve) {
+            setTimeout(resolve, interval);
+        });
+    }
 
-function delay(interval) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, interval);
-    });
-}
+    function updatePoints() {
+        var pointsUrl = config.endpoints.pointsUrl;
+        var interval = config.points.UPDATE_INTERVAL;
 
-function updatePoints(interval) {
-    var pointsUrl = config.endpoints.pointsUrl;
-    return httpGet(pointsUrl)
-        .then(Graphics.drawPoints.bind(Graphics))
-        .catch(console.error.bind(console))
-        .then(delay.bind(undefined, interval))
-        .then(updatePoints);
+        return httpGet(pointsUrl)
+            .then(canvasController.drawPoints.bind(canvasController))
+            .catch(console.error.bind(console))
+            .then(delay.bind(undefined, interval))
+            .then(updatePoints);
 
-}
+    }
 
-function startUpdate() {
-    var configUrl = config.endpoints.configUrl;
-    return httpGet(configUrl)
-        .then(updateConfig)
-        .then(getUpdateInterval)
-        .then(updatePoints)
-        .catch(console.error.bind(console));
-}
+    function startUpdate() {
+        var configUrl = config.endpoints.configUrl;
+        return httpGet(configUrl)
+            .then(updateConfig)
+            .then(updatePoints)
+            .catch(console.error.bind(console));
+    }
 
-// start
-try {
-    Graphics.init();
-    startUpdate();
-} catch (e) {
-    console.error(e);
-}
+    // start
+    var canvasController;
+    try {
+        canvasController = new CanvasController('graph');
+        startUpdate();
+    } catch (e) {
+        console.error(e);
+    }
+
+})(httpRequest);
